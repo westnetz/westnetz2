9f0f5123b1a563b3447d4d2e9cc502a05f320233
David Lamparter <equinox@diac24.net>
2016-03-11 00:07:01 +0100

    QoS: split off internal traffic

--- python/qos.py
+++ python/qos.py
@@ -5,17 +5,24 @@ import json
 import argparse
 from jinja2 import Template
 
-template = Template('''
+template = Template('''#!/bin/sh
+
+# 3999: default class (i.e. misconfiguration + ARP/LLDP/v6LL traffic)
+# 3998: internal traffic
+
 netif_base() {
 	dev="$1"
 	bw="$2"
 
 	ip link  set dev $dev up
 	ip link  set dev $dev txqueuelen 128
-	tc qdisc del dev $dev root
+	tc qdisc del dev $dev root &>/dev/null
 	tc qdisc add dev $dev root handle 1: hfsc default 3999
-	tc class add dev $dev parent 1:  classid 1:1    hfsc sc rate   $bw ul rate   $bw
-	tc class add dev $dev parent 1:1 classid 1:3999 hfsc ls rate 1Mbit ul rate 1Mbit
+	tc class add dev $dev parent 1:  classid 1:1    hfsc sc rate     $bw ul rate     $bw
+	tc class add dev $dev parent 1:1 classid 1:3999 hfsc ls rate   1Mbit ul rate   1Mbit
+	tc class add dev $dev parent 1:1 classid 1:3998 hfsc ls rate 100Mbit ul rate 750Mbit
+	tc qdisc add dev $dev parent 1:3999 handle 3999: sfq
+	tc qdisc add dev $dev parent 1:3998 handle 3998: sfq
 }
 netif_base eth0 28MBit
 netif_base ifb0 28MBit
@@ -34,19 +41,26 @@ tc_vlan() {
 	dev="$1"; shift
 	vlanid="$1"; shift
 	rate="$1"; shift
+	dir="$1"; shift
 	extra="$*"
+
 	tc class  add dev $dev parent 1:1 classid 1:$vlanid hfsc \
 		$extra ls rate 10Mbit ul rate $rate
 	tc qdisc  add dev $dev parent 1:$vlanid handle $vlanid: sfq
-	tc filter add dev $dev parent 1:0 protocol ip   priority 10 basic match meta '(' vlan eq $vlanid ')' flowid 1:$vlanid
-	tc filter add dev $dev parent 1:0 protocol ipv6 priority 20 basic match meta '(' vlan eq $vlanid ')' flowid 1:$vlanid
+
+	tc filter add dev $dev parent 1:0 protocol ip   priority 10 u32 match ip  $dir 172.19.0.0/16    flowid 1:3998
+	tc filter add dev $dev parent 1:0 protocol ip   priority 11 u32 match ip  $dir 185.142.180.0/22 flowid 1:3998
+	tc filter add dev $dev parent 1:0 protocol ipv6 priority 12 u32 match ip6 $dir 2a07:2ec0::/29   flowid 1:3998
+
+	tc filter add dev $dev parent 1:0 protocol ip   priority 20 basic match meta '(' vlan eq $vlanid ')' flowid 1:$vlanid
+	tc filter add dev $dev parent 1:0 protocol ipv6 priority 21 basic match meta '(' vlan eq $vlanid ')' flowid 1:$vlanid
 }
 
 {% for klass,info in classes|dictsort -%}
 {%  set upstream   =   info.upstream|default('21Mbit') -%}
 {%  set downstream = info.downstream|default('21Mbit') -%}
-tc_vlan ifb0 {{ klass }} {{ upstream   }} {% if info.upstream_guarantee   %} rt rate {{ info.upstream_guarantee   }} {% endif %} # up
-tc_vlan eth0 {{ klass }} {{ downstream }} {% if info.downstream_guarantee %} rt rate {{ info.downstream_guarantee }} {% endif %} # down
+tc_vlan ifb0 {{ klass }} {{ upstream   }} dst {% if info.upstream_guarantee   %} rt rate {{ info.upstream_guarantee   }} {% endif %} # up
+tc_vlan eth0 {{ klass }} {{ downstream }} src {% if info.downstream_guarantee %} rt rate {{ info.downstream_guarantee }} {% endif %} # down
 
 {% endfor %}
 ''')
