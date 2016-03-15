92202264307ead59ea1dc3a0340ed4a7e5dec696
Christian Franke <nobody@nowhere.ws>
2016-03-15 04:52:10 +0100

    Use settings in qos script, fix syntactic issues

--- python/qos.py
+++ python/qos.py
@@ -6,7 +6,7 @@ import argparse
 from jinja2 import Template
 
 template = Template('''#!/bin/sh
-
+{% set main_dev = env.MAIN_DEVICE %}
 # 3999: default class (i.e. misconfiguration + ARP/LLDP/v6LL traffic)
 # 3998: internal traffic
 
@@ -16,7 +16,7 @@ netif_base() {
 
 	ip link  set dev $dev up
 	ip link  set dev $dev txqueuelen 128
-	tc qdisc del dev $dev root &>/dev/null
+	tc qdisc del dev $dev root >/dev/null 2>&1
 	tc qdisc add dev $dev root handle 1: hfsc default 3999
 	tc class add dev $dev parent 1:  classid 1:1    hfsc sc rate     $bw ul rate     $bw
 	tc class add dev $dev parent 1:1 classid 1:3999 hfsc ls rate   1Mbit ul rate   1Mbit
@@ -24,12 +24,13 @@ netif_base() {
 	tc qdisc add dev $dev parent 1:3999 handle 3999: sfq
 	tc qdisc add dev $dev parent 1:3998 handle 3998: sfq
 }
-netif_base eth0 28MBit
-netif_base ifb0 28MBit
+netif_base {{ main_dev }} {{ env.BW_DOWNLOAD }}
+netif_base ifb0 {{ env.BW_UPLOAD }}
 
 netif_egress_ifb() {
 	dev="$1"
 	tc_match_all="u32 match u32 0 0"
+	tc qdisc  del dev $dev root >/dev/null 2>&1
 	tc qdisc  add dev $dev root handle 1: prio priomap 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
 	tc filter add dev $dev parent 1:0 protocol ip   priority 10 $tc_match_all flowid 1: action mirred egress redirect dev ifb0
 	tc filter add dev $dev parent 1:0 protocol ipv6 priority 20 $tc_match_all flowid 1: action mirred egress redirect dev ifb0
@@ -40,14 +41,16 @@ netif_egress_ifb veth-pub
 tc_inttraf() {
 	dev="$1"; shift
 	dir="$1"; shift
-
-	tc filter add dev $dev parent 1:0 protocol ip   priority 10 u32 match ip  $dir 172.19.0.0/16    flowid 1:3998
-	tc filter add dev $dev parent 1:0 protocol ip   priority 11 u32 match ip  $dir 185.142.180.0/22 flowid 1:3998
-	tc filter add dev $dev parent 1:0 protocol ipv6 priority 12 u32 match ip6 $dir 2a07:2ec0::/29   flowid 1:3998
+{% for network in env.RTR_PRIVATE_OWN_NETS.split() + env.RTR_PUBLIC_OWN_NETS_V4.split() %}
+	tc filter add dev $dev parent 1:0 protocol ip priority {{ 100 + loop.index0 }} u32 match ip  $dir {{ network }} flowid 1:3998
+{% endfor %}
+{% for network in env.RTR_PUBLIC_OWN_NETS_V6.split() %}
+	tc filter add dev $dev parent 1:0 protocol ipv6 priority {{ 200 + loop.index0 }} u32 match ip6 $dir {{ network }} flowid 1:3998
+{% endfor %}
 }
 
 tc_inttraf ifb0 dst
-tc_inttraf eth0 src
+tc_inttraf {{ main_dev }} src
 
 tc_vlan() {
 	dev="$1"; shift
@@ -59,15 +62,15 @@ tc_vlan() {
 		$extra ls rate 10Mbit ul rate $rate
 	tc qdisc  add dev $dev parent 1:$vlanid handle $vlanid: sfq
 
-	tc filter add dev $dev parent 1:0 protocol ip   priority 20 basic match meta '(' vlan eq $vlanid ')' flowid 1:$vlanid
-	tc filter add dev $dev parent 1:0 protocol ipv6 priority 21 basic match meta '(' vlan eq $vlanid ')' flowid 1:$vlanid
+	tc filter add dev $dev parent 1:0 protocol ip   priority 300 basic match meta '(' vlan eq $vlanid ')' flowid 1:$vlanid
+	tc filter add dev $dev parent 1:0 protocol ipv6 priority 400 basic match meta '(' vlan eq $vlanid ')' flowid 1:$vlanid
 }
 
 {% for klass,info in classes|dictsort -%}
 {%  set upstream   =   info.upstream|default('21Mbit') -%}
 {%  set downstream = info.downstream|default('21Mbit') -%}
-tc_vlan ifb0 {{ klass }} {{ upstream   }} dst {% if info.upstream_guarantee   %} rt rate {{ info.upstream_guarantee   }} {% endif %} # up
-tc_vlan eth0 {{ klass }} {{ downstream }} src {% if info.downstream_guarantee %} rt rate {{ info.downstream_guarantee }} {% endif %} # down
+tc_vlan ifb0 {{ klass }} {{ upstream   }} {% if info.upstream_guarantee   %} rt rate {{ info.upstream_guarantee   }} {% endif %} # up
+tc_vlan {{ main_dev }} {{ klass }} {{ downstream }} {% if info.downstream_guarantee %} rt rate {{ info.downstream_guarantee }} {% endif %} # down
 
 {% endfor %}
 ''')
@@ -86,4 +89,4 @@ spec = json.load(specfd)
 
 ##############################################################################
 
-print template.render(classes=spec['classes'])
+print template.render(classes=spec['classes'], env=os.environ)
